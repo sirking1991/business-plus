7e832a38de7a43e67fe78638e4adff0fdb094d71
User <user@example.com>
2025-08-20 08:08:54 +0800

    fix: update image path and pagination key to resolve rendering issues

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -4,7 +4,7 @@ export default function AppLogo() {
     return (
         <>
             <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <img src="businessplus_logo.png" alt="Business Plus" />
+                <img src="/businessplus_logo.png" alt="Business Plus" />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
                 <span className="mb-0.5 truncate leading-tight font-semibold">Business Plus</span>
